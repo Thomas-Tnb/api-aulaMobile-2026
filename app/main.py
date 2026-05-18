69299d5ae605c71e49c51c64de9138bfa9583a51
Thomas-Tnb <thomas.tanabe01@gmail.com>
2026-05-18 08:37:31 -0300

    enable cors

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.auth import router as auth_router
 from app.routes.jogos import router as jogos_router
+
 
 app = FastAPI(
     title="Jogos API",
     version="1.0.0"
+)
+
+# Allow all origins (any IP/domain)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
